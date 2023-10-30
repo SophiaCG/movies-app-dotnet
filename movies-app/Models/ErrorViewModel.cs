72170d075253a91d1c6b23e6af33aa982bf2a7d9
SophiaCG <sophia.gorgonio@gmail.com>
2023-10-30 18:33:41 -0400

    Initial commit

---
+++ movies-app/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace movies_app.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
---
+++ movies-app/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace movies_app.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string PosterUrl { get; set; }
+        public int Runtime { get; set; }
+    }
+}
