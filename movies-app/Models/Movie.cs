ddb27e7fda74aacc4332ec17626cde33af8cd4fb
SophiaCG <sophia.gorgonio@gmail.com>
2023-10-30 23:40:39 -0400

    Added genre mapping and fixed model

--- movies-app/Models/Movie.cs
+++ movies-app/Models/Movie.cs
@@ -7,19 +7,26 @@ namespace movies_app.Models
     public class Result
     {
         public bool Adult { get; set; }
+        [JsonProperty("backdrop_path")]
         public string? BackdropPath { get; set; }
+        [JsonProperty("genre_ids")]
         public List<int> GenreIds { get; set; } = new List<int>(); // Set to a new list to ensure non-null value
         public int Id { get; set; }
+        [JsonProperty("original_language")]
         public string? OriginalLanguage { get; set; }
+        [JsonProperty("original_title")]
         public string? OriginalTitle { get; set; }
         public string? Overview { get; set; }
         public double Popularity { get; set; }
         [JsonProperty("poster_path")]
         public string? PosterPath { get; set; }
+        [JsonProperty("release_date")]
         public DateTime ReleaseDate { get; set; }
         public string? Title { get; set; }
         public bool Video { get; set; }
+        [JsonProperty("vote_average")]
         public double VoteAverage { get; set; }
+        [JsonProperty("vote_count")]
         public int VoteCount { get; set; }
     }
 
